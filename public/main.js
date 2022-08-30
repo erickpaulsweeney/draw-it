88ffe098f3325a041577111fc793f4c7d0b4788a
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-08-30 19:43:45 +0800

    Done so far

--- public/main.js
+++ public/main.js
@@ -1,7 +1,50 @@
+const socket = io.connect("http://localhost:8000");
+const color = [
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+];
+let history = [];
+// console.log(color);
+
+socket.on("load-canvas", (data) => {
+    console.log(data);
+    history = data;
+});
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+    createCanvas(750, 750);
+    background(0);
+    noStroke();
+
+    for (let i = 0; i < history.length; i++) {
+        const item = history[i];
+        fill(...item.color);
+        ellipse(item.x, item.y, 10, 10);
+    }
+
+    socket.on("new-drawing", (data) => {
+        fill(...data.color);
+        ellipse(data.x, data.y, 10, 10);
+    });
+}
+
+const setBg = () => background(0);
+
+socket.on("clear-canvas", () => setBg());
+
+function clearCanvas() {
+    setBg();
+    socket.emit("clear-canvas");
+}
+
+function mouseDragged() {
+    let data = {
+        x: mouseX,
+        y: mouseY,
+        color: color,
+    };
+    fill(...color);
+    ellipse(mouseX, mouseY, 10, 10);
+    socket.emit("new-drawing", data);
+}
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
-
+const cors = require("cors");
+const { Server } = require("socket.io");
 const app = express();
 
+app.use(cors());
 app.use(express.static("public"));
 
+const httpServer = app.listen(8000);
 
-app.listen(8000);
+const io = new Server(httpServer);
+
+let history = [];
+
+io.on("connection", (socket) => {
+    socket.emit("load-canvas", history);
+
+    socket.on("new-drawing", (data) => {
+        socket.broadcast.emit("new-drawing", data);
+        history.push(data);
+    })
+
+    socket.on("clear-canvas", () => {
+        socket.broadcast.emit("clear-canvas");
+        history = [];
+    });
+});
